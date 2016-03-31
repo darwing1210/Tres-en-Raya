a6afa52bd4d295c3d964383997ad2ecfc1f74011
Darwing Medina <media@harvestmanagua.org>
2016-03-31 17:31:48 -0600

    Initial Commit

---
+++ Tres en RayaTests/Tres_en_RayaTests.swift
@@ -0,0 +1,36 @@
+//
+//  Tres_en_RayaTests.swift
+//  Tres en RayaTests
+//
+//  Created by Darwing Medina on 31/3/16.
+//  Copyright © 2016 Darwing Medina. All rights reserved.
+//
+
+import XCTest
+@testable import Tres_en_Raya
+
+class Tres_en_RayaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
